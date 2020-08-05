4450aad2899556764f0d1afde2cea7d24cddf020
David Favretto <david.favretto@southworks.com>
2020-08-05 18:50:47 -0300

    Implement logic to use external Data Storage

--- birthday-bot/Models/SlackUser.cs
+++ birthday-bot/Models/SlackUser.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Birthday_Bot.Models
+﻿namespace Birthday_Bot.Models
 {
     public class SlackUser
     {
         public string Id { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
-        public bool Is_Bot { get; set; }
-        public bool Is_Restricted { get; set; }
 
     }
 }
