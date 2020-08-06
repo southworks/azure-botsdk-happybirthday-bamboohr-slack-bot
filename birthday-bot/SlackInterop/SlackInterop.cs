5041e28dbd646cfe6d4b33f3ca1d181e73d5f55c
Franco Alvarez <franco.alvarez@southworks.com>
2020-08-06 11:42:44 -0300

    Add the class SlackInterop and in it add the method getChannelIdByName

---
+++ birthday-bot/SlackInterop/SlackInterop.cs
@@ -0,0 +1,41 @@
+﻿using Birthday_Bot.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Birthday_Bot
+{
+    public class SlackInterop
+    {
+        public static async Task<string> getChannelIdByName(string channelName, string slackBotToken)
+        {
+            // Here we use the Bot endpoint
+            const string botendpoint = "https://slack.com/api/conversations.list";
+            string channelId = "";
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", slackBotToken);
+                var responseMessage = httpClient.GetAsync(botendpoint).Result;
+                var responseJson = await responseMessage.Content.ReadAsStringAsync();
+                Console.WriteLine(responseJson);
+                var jsonSerializerSettings = new JsonSerializerSettings()
+                {
+                    MissingMemberHandling = MissingMemberHandling.Ignore,
+                };
+                SlackModels.SlackChannelAPIRequestResponse listChannelResponse = JsonConvert.DeserializeObject<SlackModels.SlackChannelAPIRequestResponse>(responseJson, jsonSerializerSettings);
+                if (listChannelResponse.Ok)
+                {
+                    List<SlackModels.Channel> listChannels = listChannelResponse.Channels;
+                    SlackModels.Channel channel = listChannels.Find(ch => ch.Name.Equals(channelName));
+                    if(channel != null)
+                    {
+                        channelId = channel.Id;
+                    }
+                }
+            }
+            return channelId;
+        }
+    }
+}
