4dc92fac186594cd66f2b874668fb5f0f3150469
Franco Alvarez <franco.alvarez@southworks.com>
2020-08-13 10:50:13 -0300

    Add models in order to transform the retrieve data from BambooHR API

---
+++ azure-functions/test-bamboohr-api/Models/ModifiedStructureBambooHREmployee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_bamboohr_api.Models
+{
+    class ModifiedStructureBambooHREmployee
+    {
+        public string Birthday { get; set; }
+        public string Email { get; set; }
+    }
+}
