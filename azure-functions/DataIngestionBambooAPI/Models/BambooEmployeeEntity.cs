449e240085b8d6738f37f56c2f9cd1fdc7be1b04
Franco Alvarez <franco.alvarez@southworks.com>
2020-08-27 16:46:05 -0300

    Add model to retrieve the data from the table

---
+++ azure-functions/DataIngestionBambooAPI/Models/BambooEmployeeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DataIngestionBambooAPI.Models
+{
+    class BambooEmployeeEntity : TableEntity
+    {
+
+        public BambooEmployeeEntity(int id, string birthday, string email)
+        {
+            Birthday = birthday;
+            PartitionKey = id.ToString();
+            RowKey = email;
+        }
+
+        public string Birthday { get; set; }
+
+    }
+}
