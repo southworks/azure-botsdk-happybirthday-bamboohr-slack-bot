3ee08eb2cf955345ae6653041e667b0200e0a47f
Franco Alvarez <franco.alvarez@southworks.com>
2020-08-28 09:45:14 -0300

    Refactor store methods

--- azure-functions/DataIngestionBambooAPI/BambooHRStorage.cs
+++ azure-functions/DataIngestionBambooAPI/BambooHRStorage.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataIngestionBambooAPI
 {
@@ -30,53 +31,62 @@ namespace DataIngestionBambooAPI
             }
         }
 
+        private async Task StoraDataInTableAsync(CloudStorageAccount storageAccount, List<BambooHrEmployee> employees)
+        {
+            CloudTableClient client = storageAccount.CreateCloudTableClient();
+            CloudTable table = client.GetTableReference("hrDataEmployees");
+            await table.CreateIfNotExistsAsync();
+            TableOperation insertOp;
+
+            var employeeJson = employees.Select(r => new
+            {
+                Birthday = r.DateOfBirth,
+                Email = r.WorkEmail
+            }).ToList();
+
+            foreach (var employee in employeeJson)
+            {
+                BambooEmployeeEntity employeeEntity = new BambooEmployeeEntity(1, employee.Birthday, employee.Email);
+                insertOp = TableOperation.InsertOrReplace(employeeEntity);
+                await table.ExecuteAsync(insertOp);
+            }
+        }
+
+        private async Task StoreDataInContainerAsync(CloudStorageAccount storageAccount, List<BambooHrEmployee> employees)
+        {
+            string jsonName = "hrDataEmployees.json";
+
+            CloudBlobContainer container;
+            CloudBlockBlob blob;
+            CloudBlobClient client = storageAccount.CreateCloudBlobClient();
+
+            container = client.GetContainerReference(_containerName);
+            await container.CreateIfNotExistsAsync();
+            blob = container.GetBlockBlobReference(jsonName);
+            blob.Properties.ContentType = "application/json";
+
+            var employeeJson = employees.Select(r => new
+            {
+                Birthday = r.DateOfBirth,
+                Email = r.WorkEmail
+            }).ToList().ToJson();
+
+            using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(employeeJson)))
+            {
+                await blob.UploadFromStreamAsync(stream);
+            }
+        }
+
         public async void StoreData(List<BambooHrEmployee> employees)
         {
             CloudStorageAccount storageAccount;
             storageAccount = CloudStorageAccount.Parse(_blobStorageStringConnection);
-
             if (_storageMethod == "JSON")
             {
-                string jsonName = "hrDataEmployees.json";
-                
-                CloudBlobContainer container;
-                CloudBlockBlob blob;
-                CloudBlobClient client = storageAccount.CreateCloudBlobClient();
-
-                container = client.GetContainerReference(_containerName);
-                await container.CreateIfNotExistsAsync();
-                blob = container.GetBlockBlobReference(jsonName);
-                blob.Properties.ContentType = "application/json";
-
-                var employeeJson = employees.Select(r => new
-                {
-                    Birthday = r.DateOfBirth,
-                    Email = r.WorkEmail
-                }).ToList().ToJson();
-
-                using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(employeeJson)))
-                {
-                    await blob.UploadFromStreamAsync(stream);
-                }
+                await StoreDataInContainerAsync(storageAccount, employees);
             } else
             {
-                CloudTableClient client = storageAccount.CreateCloudTableClient();
-                CloudTable table = client.GetTableReference("hrDataEmployees");
-                await table.CreateIfNotExistsAsync();
-                TableOperation insertOp;
-
-                var employeeJson = employees.Select(r => new
-                {
-                    Birthday = r.DateOfBirth,
-                    Email = r.WorkEmail
-                }).ToList();
-
-                foreach (var employee in employeeJson)
-                {
-                    BambooEmployeeEntity employeeEntity = new BambooEmployeeEntity(1, employee.Birthday, employee.Email);
-                    insertOp = TableOperation.InsertOrReplace(employeeEntity);
-                    await table.ExecuteAsync(insertOp);
-                }
+                await StoraDataInTableAsync(storageAccount, employees);
             }
 
         }
