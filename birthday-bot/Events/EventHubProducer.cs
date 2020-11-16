d4eb34e02403c6ed0df17a1966c9c76736828878
Gonza L <gonzalo.lorieto@southworks.com>
2020-11-16 17:33:03 -0300

    Implemented Event Hub producer class for publishing events.

---
+++ birthday-bot/Events/EventHubProducer.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday_Bot.Events
+{
+    public class EventHubProducer : IEventProducer
+    {
+        private readonly string _eventHubConnectionString;
+        private readonly string _eventHubName;
+
+        public EventHubProducer(IConfiguration configuration)
+        {
+            _eventHubConnectionString = configuration["EventHubConnectionString"];
+            _eventHubName = configuration["EventHubName"];
+        }
+        public async Task SendEventsAsync(string message)
+        {
+            await using (var producerClient = new EventHubProducerClient(_eventHubConnectionString, _eventHubName))
+            {
+                List<EventData> events = new List<EventData>();
+
+                events.Add(new EventData(Encoding.UTF8.GetBytes(message)));
+
+                await producerClient.SendAsync(events);
+            }
+        }
+    }
+}
---
+++ birthday-bot/Events/IEventProducer.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Birthday_Bot.Events
+{
+    public interface IEventProducer
+    {
+        Task SendEventsAsync(string message);
+    }
+}
