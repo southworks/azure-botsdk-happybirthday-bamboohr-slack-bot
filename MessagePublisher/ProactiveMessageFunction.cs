cd1c0e4cb1b85f1c9d085129614ff32dfd15ae26
Gonza L <gonzalo.lorieto@southworks.com>
2020-12-01 12:19:30 -0300

    Move Birthday Bot proactive function to separate solution.

--- MessagePublisher/ProactiveMessageFunction.cs
+++ MessagePublisher/ProactiveMessageFunction.cs
@@ -4,7 +4,7 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Birthday_Bot.ProactiveFunction
+namespace ProactiveFunctions
 {
     public static class ProactiveMessageFunction
     {
